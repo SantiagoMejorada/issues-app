3ef0da7da19a384394e13fe6a78a7ee5edd2be88
Santiago Mejorada <santiagomejorada@gmail.com>
2023-07-13 14:18:23 -0500

    LoaderSpinner component and minor designs

--- src/issues/pages/ListPage.vue
+++ src/issues/pages/ListPage.vue
@@ -1,6 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+</script>
 <template>
-  <div><span class="text-h2">ListPage</span></div>
+  <div class="row q-mb-md">
+    <div class="col-12">
+      <span class="text-h4">Github Issues</span>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 col-md-4">
+      <!-- TODO: Filtros -->
+      <!-- FilterSelector -->
+    </div>
+    <div class="col-xs-12 col-md-8">
+      <!-- TODO: Loader -->
+      <LoaderSpinner />
+
+      <!-- TODO: IssueList - Array IssueCard -->
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
